031683dfec528971aa427654a21fd0e6f52626a7
Malique Hammonds <107649903+Leekasaurus@users.noreply.github.com>
2023-09-25 16:33:01 -0400

    Cart functionality should be running

--- ecomshop/src/components/Navbar/Navbar.jsx
+++ ecomshop/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import {AppBar, ToolBar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
 import {ShoppingCart} from '@material-ui/icons';
+import {Link, useLocation} from 'reat-router-dom';
 
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
 
 const Navbar = ( {totalItems}) => {
     const classes = useStyles();
+    const location = useLocation();
+
+   
     
   return (
     <>
        <AppBar position="fixed" className={classes.appBar} color= "inherit">
        <ToolBar>
-            <Typography variant="h6" className={classes.title} color="inherit">
+            <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
             <img src={} alt="Commerce.js" height="25px" className={classes.image} />
             Commerce.js
             </Typography>
             <div className={classes.grow} />
+            {location.pathname === '/' && (
             <div className={classes.button}>
-                <IconButton aria-label="Show cart items" color="inherit">
+            <Link to="/cart"> go to cart</Link>
+                <IconButton  component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                 <Badge badgeContent={totalItems} color="secondary">
-                <ShoppingCart />
+                  <ShoppingCart />
                 </Badge>
                 </IconButton>
-            </div>
+            </div> )}
        </ToolBar>
        </AppBar>
     </>
-  )
+            )
 }
 
 export default Navbar
